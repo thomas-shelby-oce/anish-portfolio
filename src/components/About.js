50c0957d896f3579ebf2fcc37a7f38b4d68d351c
Your <you@example.com>
2025-07-09 23:57:00 +0530

    Feat: Update About Me section and fix image path for GitHub Pages

--- src/components/About.js
+++ src/components/About.js
@@ -10,10 +10,10 @@ const About = () => {
           <div className="row">
             <div className="col-lg-4 text-center">
               {/* Placeholder for professional headshot */}
-              <img src="/Anish_Picture.jpg" alt="Anish Gupta" className="img-fluid rounded-circle mb-3" />
+              <img src={process.env.PUBLIC_URL + "/Anish_Picture.jpg"} alt="Anish Gupta" className="img-fluid rounded-circle mb-3" />
             </div>
             <div className="col-lg-8">
-              <p>I am a Software Engineer with 3 years of experience. While my primary focus and passion lies in Mobile Development with SwiftUI and React Native, I also have experience in web development with React, JavaScript, and TypeScript. This combination of skills allows me to build comprehensive and consistent user experiences across both mobile and web platforms.</p>
+              <p>I am a Software Engineer with 3 years of experience. While my primary focus and passion lies in Mobile Development in iOS and React Native, I also have experience in web development with React, JavaScript, and TypeScript. This combination of skills allows me to build comprehensive and consistent user experiences across both mobile and web platforms.</p>
               <p>My experience includes architecting new applications, managing DevOps pipelines, and improving application performance and stability. I am passionate about solving complex problems, creating intuitive user interfaces, and continuously learning new technologies to drive impactful projects as a well-rounded software developer.</p>
             </div>
           </div>
